50d4ed878b078c7b6f89c308f6458431dac4a1ee
William Davidson <wj.davidson@outlook.com>
2023-10-16 20:30:49 +0200

    removed base grid calculator, toggle theme btn

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ const userTheme = localStorage.getItem("theme");
 const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 function themeCheck() {
-  if (userTheme === "dark" || (!userTheme && systemTheme))
+  if (userTheme === "dark" || (!userTheme && systemTheme)) {
     document.documentElement.classList.add("dark");
+    themeBtn.parentElement.classList.add("-translate-x-9");
+  }
 }
 
 function themeSwitch() {
   const { documentElement } = document;
-  console.log(documentElement);
   if (documentElement.classList.contains("dark")) {
     documentElement.classList.remove("dark");
     localStorage.setItem("theme", "light");
+    themeBtn.parentElement.classList.remove("-translate-x-9");
   } else {
     documentElement.classList.add("dark");
     localStorage.setItem("theme", "dark");
+    themeBtn.parentElement.classList.add("-translate-x-9");
   }
 }
 
