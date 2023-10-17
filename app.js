bdc3ea64d44ebd637dfdd08f7f3e2ac1368f6bf8
William Davidson <wj.davidson@outlook.com>
2023-10-17 18:36:31 +0200

    add numbers to current number, toggle AC and C for clear, delete single number function

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const themeBtn = document.querySelector("#themeBtn");
+const currentNumber = document.querySelector("#currentNumber");
+const prevNumber = document.querySelector("#prevNumber");
+const numberBtn = document.querySelectorAll("#number");
+const clearBtn = document.querySelector("#clear");
+const deleteBtn = document.querySelector("#delete");
+const equalBtn = document.querySelector("#equal");
 
 // Theme variables
 const userTheme = localStorage.getItem("theme");
 const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-function themeCheck() {
-  if (userTheme === "dark" || (!userTheme && systemTheme)) {
-    document.documentElement.classList.add("dark");
-    themeBtn.parentElement.classList.add("-translate-x-9");
-  }
+if (userTheme === "dark" || (!userTheme && systemTheme)) {
+  document.documentElement.classList.add("dark");
+  themeBtn.parentElement.classList.add("-translate-x-9");
 }
 
 function themeSwitch() {
@@ -24,7 +28,36 @@ function themeSwitch() {
   }
 }
 
+function addToCurrentNumber(number) {
+  const currentContent = currentNumber.textContent;
+  if (currentContent.includes(".") && number === ".") return;
+  currentNumber.textContent =
+    currentContent === "0" && number === "0"
+      ? "0"
+      : currentContent === "0" && number !== "."
+      ? number
+      : currentContent + number;
+  changeClearType();
+}
+
+function changeClearType() {
+  clearBtn.textContent = currentNumber.textContent.length > 0 ? "C" : "AC";
+}
+
+function clear(type) {
+  currentNumber.textContent = "";
+  if (type === "AC") prevNumber.textContent = "";
+  changeClearType();
+}
+
 themeBtn.addEventListener("click", () => themeSwitch());
 
-// On load
-themeCheck();
+numberBtn.forEach((btn) => {
+  btn.addEventListener("click", () => addToCurrentNumber(btn.textContent));
+});
+
+clearBtn.addEventListener("click", () => clear(clearBtn.textContent));
+
+deleteBtn.addEventListener("click", () => {
+  currentNumber.textContent = currentNumber.textContent.slice(0, -1);
+});
